49574301084eacba44a8deff0ef6b8f6f88fd74e
Raghav Kanwal <raghav.kanwal@gmail.com>
2023-07-11 16:18:18 -0700

    Footer and Header

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -3,11 +3,11 @@ import styles from './footer.module.scss'
 
 export default function Footer() {
     return (<div className={styles.footerContainer}>
-        <p>
-        <a className="mx-2 inline-block opacity-75 hover:opacity-100" href="mailto:raghav.kanwal@gmail.com">raghav.kanwal@gmail.com</a> 
-        <a className="mx-2 inline-block opacity-75 hover:opacity-100" href="instagram.com/raghavkanwal">Instagram</a> 
-        <a className="mx-2 inline-block opacity-75 hover:opacity-100" href="twitter.com/raghavkanwal">Twitter</a> 
-        <a className="mx-2 inline-block opacity-75 hover:opacity-100" href="linkedin.com/in/raghavkanwal">LinkedIn</a>
+        <p className="whitespace-nowrap overflow-x-auto max-w-[52%]">
+        <a className="mr-2 inline-block opacity-75 hover:opacity-100" href="mailto:raghav.kanwal@gmail.com">raghav.kanwal@gmail.com</a> 
+        <a className="mr-2 inline-block opacity-75 hover:opacity-100" href="instagram.com/raghavkanwal">Instagram</a> 
+        <a className="mr-2 inline-block opacity-75 hover:opacity-100" href="twitter.com/raghavkanwal">Twitter</a> 
+        <a className="mr-2 inline-block opacity-75 hover:opacity-100" href="linkedin.com/in/raghavkanwal">LinkedIn</a>
         </p>
         <p>{new Date().getFullYear()} © Raghav Kanwal</p>
     </div>)
--- components/Nav/nav.tsx
+++ components/Nav/nav.tsx
@@ -7,13 +7,18 @@ import { ThemeToggle } from '../ui/theme-toggle'
 export default function Nav() {
     return (
         <main className={`${styles.navContainer}`}>
-            { navigationMenuItems.map((item, idx) => {
-                return (<span key={idx}>
-                    <Link className={styles.link} href={item.path}>{item.name}</Link>
-                    { idx === navigationMenuItems.length -1 ? <span className="mx-2"></span> : <span className={styles.separator}>/</span> }
-                    </span>)
-            }) }
+            <div className="flex justify-between items-center">
+                <span className="font-bold">Raghav Kanwal</span>
+                <div className="flex items-center">
+                { navigationMenuItems.map((item, idx) => {
+                    return (<span key={idx}>
+                        <Link className={styles.link} href={item.path}>{item.name}</Link>
+                        { idx === navigationMenuItems.length -1 ? <span className="mx-2"></span> : <span className={styles.separator}>/</span> }
+                        </span>)
+                }) }
             <ThemeToggle />
+                </div>
+            </div>
         </main>
     )
 }
